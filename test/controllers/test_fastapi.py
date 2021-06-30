873df5eead570135035425162c74a0e88c58fd48
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-06-30 18:44:33 -0300

    TEST - Managing to run fastapi

--- test/controllers/test_fastapi.py
+++ test/controllers/test_fastapi.py
@@ -3,9 +3,17 @@ from fastapi import FastAPI
 
 # Resources.:  https://fastapi.tiangolo.com/tutorial/first-steps/
 
-app = FastAPI()
+test = FastAPI()
 
 
-@app.get("/")
+@test.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@test.get("/app/")
+async def root():
+    return {"message": "Dir changed"}
+
+@test.get("/app/thisisatest")
+async def root():
+    return {"message": "Dir changed"}
